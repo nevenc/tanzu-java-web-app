ef9c3d8d96de0359674a56e0c956dbf555aa073f
Neven Cvetkovic <ncvetkovic@vmware.com>
2022-08-25 15:43:39 +0200

    Adding DemoController - Blue

---
+++ src/main/java/com/example/springboot/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/demo")
+    public String index() {
+        return "Great Demo: BLUE";
+    }
+
+}
